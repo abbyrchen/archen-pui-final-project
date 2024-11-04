5d6e916f3eadd13b031f6daecd18aba3d100f74e
Abby Chen <archen@andrew.cmu.edu>
2024-11-04 18:50:27 -0500

    implemented header and styling

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Footer from './components/Footer';
 import FluidAnimation from './components/FluidAnimation';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ final-project/src/components/header/Header.js
@@ -0,0 +1,30 @@
+import { React } from 'react';
+import styles from './Header.module.css';
+
+// navigation bar header
+function Header() {
+    return (
+        <nav>
+            <div className={styles.logo}>
+                <h2>ABBY CHEN</h2>
+            </div>
+
+            <ul className={styles.navLinks}>
+                <li className={`${styles.navItem} ${styles.current}`}>
+                    <a href='home.jsx'>work</a>
+                </li>
+                <li className={styles.navItem}>
+                    <a href='play.jsx'>play</a>
+                </li>
+                <li className={styles.navItem}>
+                    <a href='about.jsx'>about</a>
+                </li>
+                <li className={styles.navItem}>
+                    <a href='' target='_blank'>resume</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
