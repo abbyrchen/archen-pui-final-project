61bf3d0ebbc72ea0fae886c60ece5e1331353569
Abby Chen <archen@andrew.cmu.edu>
2024-11-12 15:55:19 -0500

    added preliminary experience section

--- final-project/src/pages/home/Home.js
+++ final-project/src/pages/home/Home.js
@@ -3,12 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'; 
 import styles from './Home.module.css';
 import IntroImage from '../../assets/homepage.png';
+import ExpImage from '../../assets/experience.png';
 
 function Home() {
     return (
         <div className={styles.homePage}>
             {/* landing section w animation */}
-            <div section={styles.landingContainer}>
+            <section className={styles.landingContainer}>
                 <div className={styles.content}>
                     <img src={IntroImage} alt="Hi, I'm Abby" />
                 </div>
@@ -16,17 +17,47 @@ function Home() {
                     icon={faChevronDown} 
                     className={styles.caretIcon}
                 />
-            </div>
+            </section>
 
             {/* recent works section */}
-            <div section={styles.recentWorks}>
+            <section className={styles.recentWorks}>
 
-            </div>
+            </section>
 
             {/* experience section */}
-            <div section={styles.experience}>
-                
-            </div>
+            <section className={styles.experience}>
+                <img src={ExpImage} alt="Experience section" />
+
+                <table className={styles.experienceTable}>
+                    <tbody>
+                        <tr>
+                            <td>fall 2024</td>
+                            <td>teaching assistant</td>
+                            <td>cmu information systems</td>
+                        </tr>
+                        <tr>
+                            <td>summer 2024</td>
+                            <td>product management intern</td>
+                            <td>the home depot</td>
+                        </tr>
+                        <tr>
+                            <td>fall 2023</td>
+                            <td>ux designer</td>
+                            <td>cmu software engineering institute</td>
+                        </tr>
+                        <tr>
+                            <td>summer 2023</td>
+                            <td>software engineering intern</td>
+                            <td>icylon, llc</td>
+                        </tr>
+                        <tr>
+                            <td>2022-23</td>
+                            <td>resident assistant</td>
+                            <td>carnegie mellon university</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
         </div>
     );
 }
