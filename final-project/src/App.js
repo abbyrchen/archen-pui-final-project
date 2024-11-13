f6896fa82d356f82d209754330312ff100b9949d
Abby Chen <archen@andrew.cmu.edu>
2024-11-12 19:40:31 -0500

    completed formatting of my experience section

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
-import Footer from './components/Footer';
+import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import About from './pages/About';
 import Play from './pages/Play';
